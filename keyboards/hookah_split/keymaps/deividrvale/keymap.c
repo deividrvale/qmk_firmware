c6086579183b703345ac60293fec9927e21f5732
Deivid Vale <deividrodriguesvale@gmail.com>
2021-07-04 15:24:49 +0200

    New keymap compatible with the hookah-split.

--- keyboards/hookah_split/keymaps/deividrvale/keymap.c
+++ keyboards/hookah_split/keymaps/deividrvale/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Deivid Vale
+/* Copyright 2021 Deivid Vale
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
